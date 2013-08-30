00949a026e2a7e9d2223a25d8c343e89247b4be7
Jianfei Wang <jfwang213@gmail.com>
2013-08-30 21:22:37 +0800

    finish wx_match

---
+++ microsite/wx_match.py
@@ -0,0 +1,28 @@
+#coding:utf-8
+import logging
+from django.contrib import auth
+
+from django.contrib.contenttypes.models import ContentType
+from django.shortcuts import render_to_response, get_object_or_404, render, redirect
+from framework.models import WXAccount, Account
+from models import *
+
+logger = logging.getLogger('default')
+
+def wx_match(func):
+    def wrapper(request, *args, **kwargs):
+        active_wx_id = request.session.get('active_wx_id', 0)
+        if active_wx_id <= 0:
+            logger.debug("active_wx_id is %d" % active_wx_id)
+            return redirect('/setting')
+        wx = get_object_or_404(WXAccount, pk=active_wx_id)
+        account = Account.objects.get(user=request.user)
+        if wx.account.pk == account.pk:
+            return func(request, *args, **kwargs)
+        else:
+            logger.debug("wx account pk %d user pk %d" % (wx.account.pk, account.pk))
+            return redirect('/setting')
+    return wrapper
+
+            
+
