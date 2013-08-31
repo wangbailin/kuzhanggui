edcecead67b49f89dfce54e7ba3fe035ec4dec4e
Qiao Sun <sunqiao@wandoujia.com>
2013-08-31 15:44:27 +0800

    MOD: refactor;

--- microsite/views.py
+++ microsite/views.py
@@ -40,7 +40,7 @@ def get_apps(request):
     return apps
 
 @login_required
-def setting(request, active_tab_id = None):
+def settings(request, active_tab_id = None):
     if active_tab_id:
         active_tab_id = int(active_tab_id)
     else:
@@ -49,7 +49,7 @@ def setting(request, active_tab_id = None):
     if active_tab_id > len(tabs):
         active_tab_id = 0
     apps = get_apps(request)
-    return render(request, "setting.html", {"tabs":tabs, "active_tab_id":active_tab_id, 'page':tabs[active_tab_id][0], 'f':tabs[active_tab_id][1], 'apps':apps, 'active_side_id':-1})
+    return render(request, "settings.html", {"tabs":tabs, "active_tab_id":active_tab_id, 'page':tabs[active_tab_id][0], 'f':tabs[active_tab_id][1], 'apps':apps, 'active_side_id':-1})
 
 @login_required
 @wx_match
