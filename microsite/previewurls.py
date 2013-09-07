afd6aac084aacc72ce6189534147cf390bf55fbb
Jianfei Wang <jfwang213@gmail.com>
2013-09-07 17:43:07 +0800

    add preview for app

--- microsite/previewurls.py
+++ microsite/previewurls.py
@@ -2,4 +2,9 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('microsite.preview',
     url(r'page/(?P<page_id>\d+)', 'page'),
+    url(r'trend_item$', 'trend_item'),
+    url(r'case_item$', 'case_item'),
+    url(r'case_item/(?P<item_id>\d+)', 'case_item'),
+    url(r'product_item$', 'product_item'),
+    url(r'product_item/(?P<item_id>\d+)', 'product_item'),
 )
