35fcf473e8a61825842be782d29bc0ef43191804
Qiao Sun <sunqiao@wandoujia.com>
2013-09-09 16:57:51 +0800

    MOD: fix bugs;

--- microsite/forms.py
+++ microsite/forms.py
@@ -194,7 +194,7 @@ class ContentPageForm(ModelForm):
     icon = forms.ImageField(label=u'首页图标', widget=AjaxClearableFileInput())
     content = forms.CharField(label=u'内容', widget=CKEditorWidget())
     message_cover = forms.ImageField(label=u'消息封面', widget=AjaxClearableFileInput(), required = True, initial=consts.DEFAULT_CONTENT_COVER)
-    message_description = forms.CharField(label=u'消息内容', widget=forms.Textarea, required=True, initial=consts.DEFAULT_MSG)
+    message_description = forms.CharField(label=u'消息内容', widget=forms.Textarea(), required=True, initial=consts.DEFAULT_MSG)
     class Meta:
         model = ContentPage
         fields = (
@@ -210,7 +210,7 @@ class ContentPageForm(ModelForm):
 class LinkPageForm(ModelForm):
     icon = forms.ImageField(label=u'首页图标', widget=AjaxClearableFileInput())
     message_cover = forms.ImageField(label=u'消息封面', widget=AjaxClearableFileInput(), required = True, initial=consts.DEFAULT_LINK_COVER)
-    message_description = forms.CharField(label=u'消息内容', widget=forms.Textarea, required=True, initial=consts.DEFAULT_MSG)
+    message_description = forms.CharField(label=u'消息内容', widget=forms.Textarea(), required=True, initial=consts.DEFAULT_MSG)
     class Meta:
         model = LinkPage
         fields = (
