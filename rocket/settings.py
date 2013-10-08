711eed87cd9eb690868589e56dc968c2e8f68d06
yangchen <yangchen@SNDA-172-17-15-12.(none)>
2013-10-08 11:25:05 +0800

    add host yangchen.limijiaoyin.com

--- rocket/settings.py
+++ rocket/settings.py
@@ -30,7 +30,7 @@ CACHES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['jianfei.bestgames7.com','r.limijiaoyin.com', 'wangnan.limijiaoyin.com']
+ALLOWED_HOSTS = ['jianfei.bestgames7.com','r.limijiaoyin.com', 'wangnan.limijiaoyin.com', 'yangchen.limijiaoyin.com']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
