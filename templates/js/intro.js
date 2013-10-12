452fc894a6a28e7e201917db0687c77de33d7fa7
Chen Young <yangchen@jiaoyin.cm>
2013-10-12 15:09:31 +0800

    调整二维码的大小，统一ie和其他浏览上popover的显示效果

---
+++ templates/js/intro.js
@@ -0,0 +1,10 @@
+$(function() {
+	$(".case img ").popover({
+		html: true,
+		placement: "right",
+		// trigger: "click",
+		trigger: "hover",
+		content: "<img src='/static/img/case_lmjy_hover.jpg'/>"
+    });
+});
+
