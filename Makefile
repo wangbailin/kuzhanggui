42b7d01d82a3a168179651f5cd7bb3dbee2c8c06
Jarvys <yuhan534@126.com>
2014-03-27 18:12:58 +0800

    polish

---
+++ Makefile
@@ -0,0 +1,9 @@
+PASSWORD:=root
+
+REBUILD_SQL="drop database rocket; \
+			create database rocket;"
+rebuild_db:
+	echo $(REBUILD_SQL) | mysql -u root -p
+	./manage.py syncdb
+
+.PHONY: rebuild_db 
--- rocket/settings.py
+++ rocket/settings.py
@@ -56,7 +56,7 @@ USE_I18N = True
 USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = False
+USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.normpath(__file__)))
 # Absolute filesystem path to the directory that will hold user-uploaded files.
